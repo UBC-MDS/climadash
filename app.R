e4c761655a7b34f6a0fdb354e6ebca128dc11b16
Vikram Grewal <grewav2@student.ubc.ca>
2023-03-03 15:09:17 -0800

    added fourth plot and added formatting changes

--- app.R
+++ app.R
@@ -1,4 +1,6 @@
-# Imports
+# =================================================================== #
+# ------------------------------IMPORTS------------------------------ #
+# =================================================================== #
 library(shiny)
 library(ggplot2)
 library(tidyverse)
@@ -6,7 +8,9 @@ library(lubridate)
 options(shiny.autoreload = TRUE)
 
 
-# Importing Dataframes
+# =================================================================== #
+# --------------------------DATA PROCESSING-------------------------- #
+# =================================================================== #
 temp_df <- read.csv('https://raw.githubusercontent.com/UBC-MDS/climadash/main/data/processed/temperature_data.csv', header=TRUE)
 percip_df <- read.csv('https://raw.githubusercontent.com/UBC-MDS/climadash/main/data/processed/percipitation_data.csv', header=TRUE)
 
@@ -25,35 +29,41 @@ percip_df$month_name <- month.abb[percip_df$month]
 year_range <- unique(temp_df$year)
 year_start <- min(year_range)
 cities <- unique(temp_df$CITY)
-options <- c('Temperature', 'Percipitation')
+options <- c('Temperature (C)', 'Percipitation (mm)')
 
-# UI
+
+# =================================================================== #
+# ------------------------------SHINY UI----------------------------- #
+# =================================================================== #
 ui <- fluidPage(
+  theme = bslib::bs_theme(bootswatch = "darkly"),
   titlePanel('Climate Metrics in Canada'),
   
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("range", "Select range of years:", 
-                  min = 1940, max = 2019, 
-                  value = c(1940, 2019), step = 5),
-      uiOutput("cities"),
-      radioButtons('option', 'Select Metric', options)
-      ),
-  
-    mainPanel(
-      plotOutput("line_plot"),
-      plotOutput("line_plot2"),
-      plotOutput("month_averages")
-      
-    )
+  fluidRow(
+    column(2,
+           sliderInput("range", "Year Range:", 
+                       min = 1940, max = 2019,
+                       value = c(1940, 2019), step = 5,
+                       sep = ""),
+           uiOutput("cities"),
+           radioButtons('option', 'Select Metric', options)),
+    column(5,
+           plotOutput("line_plot"),
+           plotOutput("line_plot2")),
+    column(5,
+           plotOutput("month_averages"),
+           plotOutput("diff_plot"))
   )
 )
 
 
-# SERVER
+# =================================================================== #
+# ------------------------------SHINY SERVER------------------------- #
+# =================================================================== #
 server <- function(input, output, session) {
+  thematic::thematic_shiny()
   
-  # get the data
+  # ======Get Data for Reactivity====== #
   filtered_data <- reactive({
     if (input$option =='Temperature'){
       temp_df |> 
@@ -66,12 +76,16 @@ server <- function(input, output, session) {
     }
   })
   
-  
+  # ======Server Side of City Input====== #
   output$cities <- renderUI({
-    selectInput("cities", "Select Cities", cities, multiple = TRUE, selected = c('VANCOUVER', 'TORONTO'))
+    selectInput("cities", 
+                "Select Cities", 
+                cities, 
+                multiple = TRUE, 
+                selected = c('VANCOUVER', 'TORONTO'))
   })
   
-  # Plot 1 - annual average line plot
+  # ======Plot 1 - Annual Average Line Plot====== #
   output$line_plot <- renderPlot({
     plot1 <- filtered_data()
     
@@ -81,10 +95,44 @@ server <- function(input, output, session) {
       ggplot(aes(x = year,
                  y = year_avg,
                  color = CITY)) +
-      geom_line() + xlim(input$range[1], input$range[2])
+      geom_line() + xlim(input$range[1], input$range[2]) +
+      labs(x = "Year", 
+           y = paste("Annaul Average", input$option)) +
+      ggtitle(paste("Annual Average", 
+                    input$option)) +
+      theme(text = element_text(size=12),
+            plot.title = element_text(face = "bold"),
+            axis.title = element_text(face = "bold"),
+            legend.position="none",
+            axis.text.x = element_text(angle = 90))
   })
   
-  # Plot 3 - annual average line plot with trendline
+  # ======Plot 2 - Average Monthly Bar Plot====== #
+  output$month_averages <- renderPlot({
+    dff <- filtered_data()
+    dff |>
+      group_by(month_name, CITY) |>
+      summarise(month = mean(month),
+                mean=mean(value)) |>
+      arrange(month) |> ggplot(aes(x=reorder(month_name, month), y=mean, fill=CITY)) +
+      geom_col(stat="identity", color="white", position=position_dodge()) +
+      scale_y_continuous(breaks = seq(-30, 40, by = 5))+
+      theme(panel.grid.major.y = element_line(color = "grey",
+                                              size = 0.5,
+                                              linetype = 2))+ 
+      ggtitle(paste0("Monthly Average of ", 
+                     input$option)) +
+      xlab("Month") + 
+      ylab(paste("Average Monthly",
+                 input$option))+
+      theme(text = element_text(size=12),
+            plot.title = element_text(face = "bold"),
+            axis.title = element_text(face = "bold"),
+            axis.text.x = element_text(angle = 90))
+    
+  })
+  
+  # ======Plot 3 - Annual Average Line Plot with Trendline====== #
   output$line_plot2 <- renderPlot({
     plot3 <- filtered_data()
     
@@ -97,38 +145,42 @@ server <- function(input, output, session) {
       geom_point() + 
       geom_smooth(se = TRUE, span = (input$range[2] - input$range[1])/10) +
       xlim(input$range[1], input$range[2]) +
-      labs(x = "Year", y = paste("Annaul Average", input$option)) +
-      ggtitle(paste("Scatter Plot with Trendline for Selected Cities' Annual Average", 
-                    input$option, 
-                    '\n (from', input$range[1], 'to', input$range[2], ')')) +
+      labs(x = "Year", 
+           y = paste("Annaul Average", input$option)) +
+      ggtitle(paste("Trendline for Selected Cities' Annual Average", 
+                    input$option)) +
       theme(text = element_text(size=12),
             plot.title = element_text(face = "bold"),
             axis.title = element_text(face = "bold"),
-            legend.title = element_text(face = "bold"))
+            legend.position="none",
+            axis.text.x = element_text(angle = 90))
   })
   
-  output$month_averages <- renderPlot({
-    dff <- filtered_data()
-    dff |>
-        group_by(month_name, CITY) |>
-        summarise(month = mean(month),
-                  mean=mean(value)) |>
-        arrange(month) |> ggplot(aes(x=reorder(month_name, month), y=mean, fill=CITY)) +
-      geom_col(stat="identity", color="white", position=position_dodge()) +
-      scale_y_continuous(breaks = seq(-30, 40, by = 5))+
-      theme(panel.grid.major.y = element_line(color = "grey",
-                                              size = 0.5,
-                                              linetype = 2))+ 
-      ggtitle(paste0("Monthly Average of ", input$option, "\" from ", input$range[1], " to ", input$range[2])) +
-      xlab("") + ylab("CELSIUS")+ 
+  # ======Plot 4 - Yearly Maximum Difference Line Plot====== #
+  output$diff_plot <- renderPlot({
+    plot3 <- filtered_data()
+    
+    plot3 |> 
+      group_by(year, CITY) |> 
+      summarise(max_diff = max(value)-min(value), .groups = 'drop') |>
+      ggplot(aes(x = year,
+                 y = max_diff,
+                 color = CITY)) +
+      geom_line() +
+      xlim(input$range[1], input$range[2]) +
+      labs(x = "Year", y = paste("Max Difference in", input$option)) +
+      ggtitle(paste("Yearly Difference Between Highest and Lowest", 
+                    input$option)) +
       theme(text = element_text(size=12),
             plot.title = element_text(face = "bold"),
             axis.title = element_text(face = "bold"),
-            legend.title = element_text(face = "bold"))
-  
+            legend.position="none",
+            axis.text.x = element_text(angle = 90))
   })
   
 }
 
-# Run the application
+# =================================================================== #
+# ------------------------------SHINY RUN---------------------------- #
+# =================================================================== #
 shinyApp(ui = ui, server = server)
