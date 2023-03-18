0add28793e52546e8e38cf98e8419b7ca618c1f8
kellywujy <kellywu817@outlook.com>
2023-03-17 23:08:46 -0700

    test added

---
+++ tests/testthat/test-shinytest2.R
@@ -0,0 +1,26 @@
+library(shinytest2)
+
+test_that("{shinytest2} recording: map_value_test", {
+  app <- AppDriver$new(variant = platform_variant(), name = "map_value_test", height = 764, 
+      width = 1139)
+  app$expect_values(output = "map")
+})
+
+
+
+test_that("{shinytest2} recording: slider_test", {
+  app <- AppDriver$new(name = "slider_test", height = 764, width = 1139)
+  app$set_inputs(range = c(1985, 2019))
+  app$expect_values(output = "line_plot")
+})
+
+
+
+test_that("{shinytest2} recording: global_test", {
+  app <- AppDriver$new(name = "global_test", height = 764, width = 1139)
+  app$set_inputs(option = "Precipitation (mm)")
+  app$set_inputs(cities = "EDMONTON")
+  app$set_inputs(range = c(1960, 2000))
+  app$expect_values()
+})
+
